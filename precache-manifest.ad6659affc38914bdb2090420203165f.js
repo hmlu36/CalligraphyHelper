b2750757e0a3cb33383318793825652bbf3c7941
Joshua36 <hmlu36@gmail.com>
2019-07-12 17:45:08 +0800

    Updates

---
+++ precache-manifest.ad6659affc38914bdb2090420203165f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc173542d9a578d22da5fd165de52ec",
+    "url": "/CalligraphyHelper/index.html"
+  },
+  {
+    "revision": "1f07de98cab069d8f085",
+    "url": "/CalligraphyHelper/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/CalligraphyHelper/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1f07de98cab069d8f085",
+    "url": "/CalligraphyHelper/static/js/main.2b920a4c.chunk.js"
+  },
+  {
+    "revision": "01267c42851a620cc13c",
+    "url": "/CalligraphyHelper/static/js/runtime~main.699ce923.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CalligraphyHelper/static/media/logo.5d5d9eef.svg"
+  }
+]);
